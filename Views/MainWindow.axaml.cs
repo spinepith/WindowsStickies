0738fa4a5596f0c4851c10b24a19db1d46350831
Vasilii Dubenskii <gamerguy40@mail.ru>
2026-09-08 02:43:28 +0300

    square window corners

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,10 +1,37 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using System.Runtime.InteropServices;
+using System;
 
 namespace WindowsStickies.Views;
 
 public partial class MainWindow : Window {
     public MainWindow() {
         InitializeComponent();
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+            this.Opened += (s, e) => {
+                if (TryGetPlatformHandle()?.Handle is IntPtr hwnd) {
+                    SetWindowCornerPreference(hwnd, DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_DONOTROUND);
+                }
+            };
+        }
+    }
+
+    [DllImport("dwmapi.dll")]
+    private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+    private const int DWMWA_WINDOW_CORNER_PREFERENCE = 33;
+
+    private enum DWM_WINDOW_CORNER_PREFERENCE {
+        DWMWCP_DEFAULT = 0,
+        DWMWCP_DONOTROUND = 1,
+        DWMWCP_ROUND = 2,
+        DWMWCP_ROUNDSMALL = 3
+    }
+
+    private static void SetWindowCornerPreference(IntPtr hwnd, DWM_WINDOW_CORNER_PREFERENCE preference) {
+        int pref = (int)preference;
+        DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, ref pref, sizeof(int));
     }
 }
